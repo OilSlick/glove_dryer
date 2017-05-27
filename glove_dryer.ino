20c83ab6d0aac3d9c04b2524cc0e9903c1cbe2c7
OilSlick <michael@oilslickcoffee.com>
2017-05-27 13:37:19 +0800

    remove neopixels

--- glove_dryer.ino
+++ glove_dryer.ino
@@ -1,25 +1,24 @@
-// #include "TH02_dev.h"        // Needed for Grove TH02 sensor
-// #include "Arduino.h"         //(I don't remember why I include this)
-// #include "Wire.h"            //Permits I2C comms but TH02_dev.h includes functionality
 #include "DHT.h"                //Permits DHT usage
 #include "RunningAverage.h"     //Calculates running average for sensor correlation.
 #include "LowPower.h"           //Provides sleep/idle/powerdown functions
-#include <Adafruit_NeoPixel.h>  //Drives the Neopixel LEDs
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
-#define ledPIN 6
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(3, ledPIN, NEO_GRB + NEO_KHZ800);
-
 //Running average vars
 RunningAverage myRA(10);
-int samples = 0;                //Count number of samples taken
+int samples = 0;                //Count number of RA samples taken
 float previousRA = 0;           //Track previous running average (RA)
+
+//For RGB LED
+const int redLED = 9;
+const int greenLED = 10;
+const int blueLED = 11;
+
 int humidityOUT = 0;            //Humidity outside of glove
 int humidityOUTcorrelated = 0;  //Correlated DHT value
 int humidityGLOVE = 0;          //Humidity inside of glove
-int humidityDIFF = 0;           //Difference between TH02 and DHT11 sensors
+int humidityDIFF = 0;           //Difference between DHT11 sensors
 
 const int TIP120pin = 5;        //Base pin of TIP120 transistor
 
@@ -33,9 +32,10 @@ DHT dhtOut;
 
 void setup()
 {
-  //NeoPixels setup
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  //For RGB LED
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(blueLED, OUTPUT);
   
   //DHT sensor setup
   dhtGlove.setup(A0);
@@ -51,29 +51,7 @@ void setup()
   delay(dhtOut.getMinimumSamplingPeriod());   //play nice with DHT sensor
 
   Serial.println("Beginning sensor correlation");
-
-  while (millis() < 10000)
-  {
-  Serial.print("Millis: ");
-  Serial.println(millis());
-  humidityGLOVE = dhtGlove.getHumidity();
-  Serial.print("TH02: ");
-  Serial.print(humidityGLOVE);
-  humidityOUT = dhtOut.getHumidity();
-  Serial.print("\t DHT: ");
-  Serial.print(humidityOUT);
-  humidityDIFF = humidityGLOVE - humidityOUT;
-  Serial.print("\t Diff: ");
-  Serial.println(humidityDIFF);
-  humidityOUTcorrelated = humidityOUT + myRA.getAverage();
-  Serial.print("Correlated Room Humidity: ");
-  Serial.println(humidityOUTcorrelated);
-
-  myRA.addValue(humidityDIFF);        //Add diff to running average
-  samples++;
-
-  delay(1000);
-  }
+  correlateSensors();
 }
  
 void loop()
@@ -84,7 +62,6 @@ void loop()
   humidityOUTcorrelated = humidityOUT + myRA.getAverage();
   
   //Track historical high and low readings for display on LCD
-   
   if (humidityGLOVE > HIGHREADING)
   {
     HIGHREADING=humidityGLOVE;
@@ -100,7 +77,7 @@ void loop()
     DISPLAYSERIAL();
     Serial.println("Fan on");
     analogWrite(TIP120pin, 255);            //Turn fan on "full" (255 = full)   
-    rainbow(20);                            //Display rainbow fade with pixels
+    //rainbow(20);                            //Display rainbow fade with pixels
   }
   else 
   {
@@ -108,9 +85,7 @@ void loop()
     Serial.println("Fan off");
     analogWrite(TIP120pin, 0); // Fan off
     Serial.println("Power nap...");
-    colorWipe(strip.Color(0, 0, 0), 50);    // Black/off
-    strip.show();                           //All pixels off
-    delay(100);                             //Delay the powernap
+    delay(100);                             //Delay the powernap 
     LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
     delay(2000);                            //Delay sensor reading until all powered back on
   }
@@ -136,40 +111,47 @@ void DISPLAYSERIAL()
   Serial.println(LOWREADING);
 }
 
-//NeoPixel Functions
+void correlateSensors()
+{
+  while (millis() < 10000)
+  {
+  
+  humidityGLOVE = dhtGlove.getHumidity();
+  humidityOUT = dhtOut.getHumidity();
+  humidityDIFF = humidityGLOVE - humidityOUT;
+  humidityOUTcorrelated = humidityOUT + myRA.getAverage();
+  if ( Serial ) {
+    Serial.print("Millis: ");
+    Serial.println(millis());
+    Serial.print("DHT Glove: ");
+    Serial.print(humidityGLOVE);
+    Serial.print("\t DHT Out: ");
+    Serial.print(humidityOUT);
+    Serial.print("\t Diff: ");
+    Serial.println(humidityDIFF);
+    Serial.print("Correlated Room Humidity: ");
+    Serial.println(humidityOUTcorrelated);
+  }
 
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
+  //Blink LED
+  setColor(0, 0, 16);  // blue
+  delay(50);
+  setColor(0, 0, 0);      // no LED
+  delay(50);
+  setColor(0, 0, 16);  // blue
+  delay(50);
+  setColor(0, 0, 0);      // no LED
 
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
+  myRA.addValue(humidityDIFF);        //Add diff to running average
+  samples++;
+
+  delay(1000);
   }
 }
 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
-
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
+void setColor(int red, int green, int blue)
+{
+  analogWrite(redLED, red);
+  analogWrite(greenLED, green);
+  analogWrite(blueLED, blue);  
 }
